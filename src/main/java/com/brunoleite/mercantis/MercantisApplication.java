4255ed26d0a4a72a19acadeadf05bc537f7d91ff
bruno2leite <bpl10x@gmail.com>
2025-09-24 20:23:31 -0300

    Primeiro commit - estrutura inicial do projeto

---
+++ src/main/java/com/brunoleite/mercantis/MercantisApplication.java
@@ -0,0 +1,13 @@
+package com.brunoleite.mercantis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercantisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercantisApplication.class, args);
+	}
+
+}
